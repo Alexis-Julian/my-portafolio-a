3d5965a20e5e683f233d2ce18d45eea8180b5592
unknown <alexisjrojas@hotmail.es>
2025-01-22 00:41:43 -0300

    agregado de secciones

---
+++ src/components/Test.js
@@ -0,0 +1,12 @@
+identity = {
+   fullname: 'Alexis Julian Rojas',
+   age: '21',
+   birthdate: '01/03/2003',
+   speciality: 'Frontend',
+   hobbies: [
+      'Caminar',
+      'Peliculas y series',
+      'VideoJuegos',
+      'Ver streams de programacion',
+   ],
+};
